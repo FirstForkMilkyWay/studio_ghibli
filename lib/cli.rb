931ad1a76e76027971fd7a7d3c337eac39772483
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-21 16:03:58 +0000

    asking user for movie choice method

--- lib/cli.rb
+++ lib/cli.rb
@@ -15,6 +15,7 @@ class ClI
     if user_input == 'yes' || user_input == 'y'
         puts "Good Choice!"
         display_list_of_movies
+        ask_user_for_movie_choice
     end
 
   end
@@ -23,6 +24,18 @@ class ClI
     Movies.all.each.with_index(1) do |movie, index|
       puts "#{index}. #{movie.title}"
     end
+  end
+
+  def ask_user_for_movie_choice
+    puts "Enter the number of the movie you'd like to know more about"
+    index = gets.strip.to_i - 1
+
+    until index.between?(0, Movies.all.length - 1)
+      puts "Sorry invalid input. Choose a valid number."
+      index = gets.strip.to_i - 1 
+    end
+
+    movie_instance = Movies.all[index]
   end
 
 
